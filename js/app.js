2bf18ff1c84c70478790425bd84fb7e9ed75984c
Jon Hammond <jonh1016@gmail.com>
2016-04-02 03:00:49 -0600

    Added controllers for navBar and for new posts with default values. Still need to add dropdown form for new posts, and search functionality. Added css.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 // sample angular code
 
-var app = angular.module('myApp', []);
+var app = angular.module('redditClone', []);
 
-app.controller('myController', ['$scope', function($scope) {
-  $scope.greeting = "Hello World!";
+app.controller('navBarController', ['$scope', function($scope) {
+
+}]);
+
+app.controller('postsController', ['$scope', function($scope) {
+  $scope.postsObject = [
+  {title: "Outside Aspen, CO", voteCount: 10, description: ipsum, imgUrl:"http://l7.alamy.com/zooms/cdab621b79be43239a18ac9992cfb427/wildflowers-rim-creek-trail-snowmass-village-aspen-colorado-usa-dc2hd8.jpg", postedBy: "Ned Stark", date: 1459583635, commentCount: 2},
+  {title: "Cliffs of Moher", voteCount: 5, description: ipsum, imgUrl:"http://i.imgur.com/Mycvbzf.jpg", postedBy: "Cersei Lannister", date: 1459583635, commentCount: 1},
+  {title: "Olympic National Park", voteCount: 2, description: ipsum, imgUrl:"http://outdoor-society.com/cms/wp-content/uploads/2015/03/Wildflowers-and-Lake-Constance-in-Olympic-National-Park.jpg", postedBy: "Bear Grylls", date: 1459583635, commentCount: 2}
+  ];
 }]);

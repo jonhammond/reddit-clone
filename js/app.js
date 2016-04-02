6423c0f4f67074e627fc04a43d163014612e3db2
Jon Hammond <jonh1016@gmail.com>
2016-04-02 15:28:39 -0600

    Fixed sort

--- js/app.js
+++ js/app.js
@@ -2,14 +2,13 @@
 
 var app = angular.module('redditClone', []);
 
-app.controller('navBarController', ['$scope', function($scope) {
+app.controller('postsController', ['$scope', '$rootScope', function($scope, $rootScope) {
+  $scope.newForm = 0;
+  $scope.sorting = '';
 
-}]);
-
-app.controller('postsController', ['$scope', function($scope) {
   $scope.postsObject = [
-  {title: "Outside Aspen, CO", voteCount: 10, description: ipsum, imgUrl:"http://l7.alamy.com/zooms/cdab621b79be43239a18ac9992cfb427/wildflowers-rim-creek-trail-snowmass-village-aspen-colorado-usa-dc2hd8.jpg", postedBy: "Ned Stark", date: 1459583635, commentCount: 2},
-  {title: "Cliffs of Moher", voteCount: 5, description: ipsum, imgUrl:"http://i.imgur.com/Mycvbzf.jpg", postedBy: "Cersei Lannister", date: 1459583635, commentCount: 1},
-  {title: "Olympic National Park", voteCount: 2, description: ipsum, imgUrl:"http://outdoor-society.com/cms/wp-content/uploads/2015/03/Wildflowers-and-Lake-Constance-in-Olympic-National-Park.jpg", postedBy: "Bear Grylls", date: 1459583635, commentCount: 2}
+  {title: "Outside Aspen, CO", voteCount: 10, description: ipsum, imgUrl:"http://l7.alamy.com/zooms/cdab621b79be43239a18ac9992cfb427/wildflowers-rim-creek-trail-snowmass-village-aspen-colorado-usa-dc2hd8.jpg", postedBy: "Ned Stark", date: 1425928936235, commentCount: 2},
+  {title: "Cliffs of Moher", voteCount: 5, description: ipsum, imgUrl:"http://i.imgur.com/Mycvbzf.jpg", postedBy: "Cersei Lannister", date: 1458333343667, commentCount: 1},
+  {title: "Olympic National Park", voteCount: 2, description: ipsum, imgUrl:"http://outdoor-society.com/cms/wp-content/uploads/2015/03/Wildflowers-and-Lake-Constance-in-Olympic-National-Park.jpg", postedBy: "Bear Grylls", date: 1431634323335, commentCount: 2}
   ];
 }]);
